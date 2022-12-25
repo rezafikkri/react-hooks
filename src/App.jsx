fb1e832ae39d34fcaf7aa0b143083d3a4c29ca44
rezafikkri <fikkri.reza@gmail.com>
2022-12-25 17:20:12 +0700

    feat: useContext hooks

--- src/App.jsx
+++ src/App.jsx
@@ -1,21 +1,25 @@
-import { useEffect, useState } from 'react'
+import { useContext, useEffect, useState } from 'react'
 import reactLogo from './assets/react.svg'
 import './App.css'
+import ThemeContext from './ThemeContext';
 
 function App() {
   const [name, setName] = useState('Reza Sariful Fikri');
   const [imageSrc, setImageSrc] = useState('https://cdn.pixabay.com/photo/2015/12/05/06/20/kid-1077793_960_720.jpg');
+  const theme = useContext(ThemeContext);
 
   useEffect(() => {
     document.title = name;
   }, [name]);
 
   return (
-    <div className="card">
-      <input type="text" value={name} onChange={(e) => setName(e.target.value)} />
-      <input type="text" value={imageSrc} onChange={(e) => setImageSrc(e.target.value)} />
-      <p>{name}</p>
-      <img src={imageSrc} alt={imageSrc} />
+    <div className={theme}>
+      <div className="card">
+        <input type="text" value={name} onChange={(e) => setName(e.target.value)} />
+        <input type="text" value={imageSrc} onChange={(e) => setImageSrc(e.target.value)} />
+        <p>{name}</p>
+        <img src={imageSrc} alt={imageSrc} />
+      </div>
     </div>
   );
 }
--- src/main.jsx
+++ src/main.jsx
@@ -2,9 +2,12 @@ import React from 'react'
 import ReactDOM from 'react-dom/client'
 import App from './App'
 import './index.css'
+import ThemeContext from './ThemeContext'
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
-    <App />
+    <ThemeContext.Provider value="light">
+      <App />
+    </ThemeContext.Provider>
   </React.StrictMode>,
 )
